d738d4022c59f987e63069af7778ad41d2731e66
thundertech9 <81930771+thundertech9@users.noreply.github.com>
2021-10-01 13:17:55 +0530

    Create stats.py

---
+++ handlers/stats.py
@@ -0,0 +1,16 @@
+@Client.on_message(command("gstats"))
+@sudo_users_only
+async def botstats(_, message: Message):
+    total, used, free = shutil.disk_usage(".")
+    total = humanbytes(total)
+    used = humanbytes(used)
+    free = humanbytes(free)
+    cpu_usage = psutil.cpu_percent()
+    ram_usage = psutil.virtual_memory().percent
+    disk_usage = psutil.disk_usage("/").percent
+    total_users = await db.total_users_count()
+    await message.reply_text(
+        text=f"**📊 stats of @{BOT_USERNAME}** \n\n**🤖 bot version:** `v6.5` \n\n**🙎🏼 total users:** \n » **on bot pm:** `{total_users}` \n\n**💾 disk usage:** \n » **disk space:** `{total}` \n » **used:** `{used}({disk_usage}%)` \n » **free:** `{free}` \n\n**🎛 hardware usage:** \n » **CPU usage:** `{cpu_usage}%` \n » **RAM usage:** `{ram_usage}%`",
+        parse_mode="Markdown",
+        quote=True,
+    )
